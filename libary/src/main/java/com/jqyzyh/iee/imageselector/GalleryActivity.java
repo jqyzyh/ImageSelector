0793ffa6754b468eff81b7e42d2ab6210e93c378
jqyzyh <jqyzyh@gmail.com>
2017-03-29 13:38:43 +0800

    commit yh

---
+++ libary/src/main/java/com/jqyzyh/iee/imageselector/GalleryActivity.java
@@ -0,0 +1,12 @@
+package com.jqyzyh.iee.imageselector;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * @author jqyzyh
+ */
+
+public class GalleryActivity extends FragmentActivity {
+
+
+}
